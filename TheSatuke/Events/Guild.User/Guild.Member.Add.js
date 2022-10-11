42224ea0913bd2cdee5ae54cdbc8a1f346f9f0de
satuke <52047528+TheSatuke@users.noreply.github.com>
2022-10-11 20:06:39 +0300

    Update Guild.Member.Add.js

--- TheSatuke/Events/Guild.User/Guild.Member.Add.js
+++ TheSatuke/Events/Guild.User/Guild.Member.Add.js
@@ -14,13 +14,3 @@ module.exports = async (member, message) => {
 module.exports.config = {
     Event: "guildMemberAdd"
 }
-
-/*
-satukeallah.send(`\`${user.tag} [${user.id}]\``)
-, iconURL: 'https://media.discordapp.net/attachments/986287323283079178/1026883075998744607/a8e1460c209229b1ee3a4cc6142c2041.jpg', url: 'https://discord.gg/1978' 
-
-var ACreated = await client.users.fetch("1008733425341042858");
-let logChannel = member.guild.channels.cache.find(r => r.name === "log");
-user.send({embeds: [new MessageEmbed().setDescription(`\`\`\`Maalesef ki URL'n bize düşmüş.. Ne yazık ki bu konuda sana yardımcı olamayacağım!\`\`\``).setColor("ffffff").setTitle('118 Kingdoms').setURL('https://discord.gg/1937')]})
-logChannel.send({embeds: [new MessageEmbed().setColor(`ffffff`).setDescription(`\`\`\`1978 URL'sini kontrol ediyor 🤨:\`\`\`\n\`${user.tag} [${user.id}]\`\n\`Giriş Zamanı:\` **<t:${Math.floor(Date.now() / 1000)}:R>**`).setAuthor({ name: '1978 UNRİVALED KİNGS', iconURL: 'https://media.discordapp.net/attachments/986287323283079178/1026883075998744607/a8e1460c209229b1ee3a4cc6142c2041.jpg'})]})
-*/
