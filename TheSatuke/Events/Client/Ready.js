7bdf91ebca2c78a6956330c4ef591a22066e61d9
satuke <52047528+TheSatuke@users.noreply.github.com>
2022-10-11 20:07:15 +0300

    Update Ready.js

--- TheSatuke/Events/Client/Ready.js
+++ TheSatuke/Events/Client/Ready.js
@@ -3,9 +3,8 @@ const { joinVoiceChannel } = require("@discordjs/voice");
 const client = global.client;
 
 module.exports = (member) => {
-/*    client.user.setPresence({ activity: { name: ("development by nabiz"), type: "WATCHING" }, status: "online" });
-    client.user.setPresence({ activities: [{ name: "development by nabiz", type: "WATCHING" }], status: "online" });
-*/
+    client.user.setPresence({ activity: { name: ( Config.Status ), type: "WATCHING" }, status: "online" });
+    client.user.setPresence({ activities: [{ name: Config.Status, type: "WATCHING" }], status: "online" });
 }
 
 module.exports.config = {
